7ffc4e75081b144afb7cce98413028eb7d37c656
SpatLyu <lyu.geosocial@gmail.com>
2024-09-06 23:43:09 +0800

    update logo

--- geocomplexity_logo.R
+++ geocomplexity_logo.R
@@ -13,15 +13,18 @@ sticker(
   package = "geocomplexity",
   p_family = "ShineTypewriter",
   p_size = 16.5,
-  p_color = ggplot2::alpha("#8297a0",.85),#ggplot2::alpha("#3e3221",.75),
+  p_color = ggplot2::alpha("#8297a0",.95),#ggplot2::alpha("#3e3221",.75),
   p_x = 1.01,
   p_y = 1.0,
   dpi = 300,
   asp = 1,
   h_size = 1.75,
-  h_color = ggplot2::alpha("#8297a0",.85),
+  h_color = ggplot2::alpha("#8297a0",.95),
   h_fill = ggplot2::alpha('#f0f9fe',0.75),
   white_around_sticker = F,
+  url = "https://ausgis.github.io/geocomplexity",
+  u_color = ggplot2::alpha("#8297a0",.95),
+  u_size = 4.2,
   filename = "geocomplexity_logo.png"
 )
 
