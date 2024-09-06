f3e7c53ac509edec315289b476a2ae3d7ac98b63
SpatLyu <lyu.geosocial@gmail.com>
2024-09-06 23:10:34 +0800

    update

--- geocomplexity_logo.R
+++ geocomplexity_logo.R
@@ -5,22 +5,22 @@ library(hexSticker)
 library(magick)
 
 sticker(
-  subplot = "./gislayers.jpg",
-  s_x = 1,
-  s_y = 1.15,
-  s_width = .7,
-  s_height = .7,
+  subplot = "./complexity.png",
+  s_x = 0.985,
+  s_y = 0.995,
+  s_width = .8,
+  s_height = .8,
   package = "geocomplexity",
   p_family = "ShineTypewriter",
-  p_size = 15,
-  p_color = ggplot2::alpha("#8297a0",.85),#ggplot2::alpha("#3e3221",.75),
+  p_size = 7.5,
+  p_color = ggplot2::alpha("#8297a0",.75),#ggplot2::alpha("#3e3221",.75),
   p_x = 1.01,
-  p_y = 0.49,
+  p_y = 1.05,
   dpi = 300,
   asp = 1,
   h_size = 1.75,
   h_color = ggplot2::alpha("#8297a0",.85),
-  h_fill = ggplot2::alpha('#ffffff',0),
+  h_fill = ggplot2::alpha('#f0f9fe',0.85),
   white_around_sticker = F,
   filename = "geocomplexity_logo.png"
 )
